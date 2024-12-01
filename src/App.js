863a2d7c4ad2e2aa7f2a89f7cf21dd3e2de9bad9
Hassaan Ahmed <hassaankh8@gmail.com>
2024-12-01 15:44:04 +0500

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import QRCode from 'react-qr-code';
 
 function App() {
+
+  const [qrtext, setqrtext] = useState('')
+  const [qrshow, setqrshow] = useState('')
+
+  const handleQRChange = (e) => {
+    setqrtext(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setqrshow(qrtext)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <h1 className='heading'>QR Code Generator</h1>
+      {qrshow?(
+      <div style={{ padding: "16px" }}>
+        <QRCode value={qrshow} fgColor='#36454f' bgColor='#efeee9'/>
+      </div>
+      ):(
+        <div style={{ padding: "16px" }}>
+        <QRCode value={"Welcome to QR Code Generator."} fgColor='#36454f' bgColor='#efeee9'/>
+      </div>
+      )}
+      <input type='text' className='QRText' placeholder='Enter text to generate QR Code...' value={qrtext} onChange={handleQRChange}/>
+      <button className='QRBtn' onClick={()=>{handleSubmit()}}>SUBMIT</button>
     </div>
   );
 }
